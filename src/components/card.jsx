22320acec97e053dff13bc930d8be86b2a9b710e
MichaelVelasquez15 <80552624+MichaelVelasquez15@users.noreply.github.com>
2023-10-05 22:09:28 -0500

    nav

--- src/components/card.jsx
+++ src/components/card.jsx
@@ -1,3 +1,13 @@
-function Card() {}
+function Card() {
+  return (
+    <>
+      <div className="fondo">
+        {" "}
+        <input type="text" placeholder="Add Guest" />
+        <i className="fa-solid fa-magnifying-glass"></i>
+      </div>
+    </>
+  );
+}
 
 export default Card;

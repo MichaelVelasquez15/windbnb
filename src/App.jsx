3734b0fb144242cf05099e6197b13a1a34c13243
MichaelVelasquez15 <80552624+MichaelVelasquez15@users.noreply.github.com>
2023-10-09 07:42:05 -0500

    Final

--- src/App.jsx
+++ src/App.jsx
@@ -1,43 +1,12 @@
-import { useEffect, useState } from "react";
 import "./App.css";
+import Nav from "./components/Nav";
 import Card from "./components/card";
 
 function App() {
-  // La variable data es la que va a almacenar los datos de "stays.json" y setData nos ayudará a guardar esos datos en esa variable. Es necesario que inicialicemos esa variable como un array vacío para evitar errores.
-  const [data, setData] = useState([]);
-
-  // Función para traer los datos de "stays.json".
-  const getData = async () => {
-    // Esta sentencia try-catch sirve para manejar los errores que se podrían generar al importar los datos de "stays.json".
-    try {
-      const res = await fetch("stays.json");
-      const resJson = await res.json();
-      // Aquí guardamos los datos de "stays.json" en la variable data.
-      setData(resJson);
-    } catch (error) {
-      console.log(error);
-    }
-  };
-
-  // Este Hook te va a ejecutar la función getData cada vez que la página se renderice.
-  useEffect(() => {
-    getData();
-  }, []);
-
-  // Puedes ver la variable data en consola.
-  console.log(data);
   return (
     <>
-      <Card />;
-      {/* Aquí te dejo un ejemplo de cómo podrías imprimir varios elementos a la vez. */}
-      {data.map((el, i) => {
-        return (
-          <>
-            <h1>{el.city}</h1>
-            <img src={el.photo} width={100} height={100}></img>
-          </>
-        );
-      })}
+      <Nav />
+      <Card />
     </>
   );
 }
@@ -45,5 +14,4 @@ function App() {
 export default App;
 
 {
-  /* <h1 key={i}>{el.city}</h1>; */
 }
--- src/components/card.jsx
+++ src/components/card.jsx
@@ -1,11 +1,60 @@
+import { useEffect, useState } from "react";
+
 function Card() {
+  const [data, setData] = useState([]);
+
+  const getData = async () => {
+    // Esta sentencia try-catch sirve para manejar los errores que se podrían generar al importar los datos de "stays.json".
+    try {
+      const res = await fetch("stays.json");
+      const resJson = await res.json();
+      // Aquí guardamos los datos de "stays.json" en la variable data.
+      setData(resJson);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    getData();
+  }, []);
+
   return (
     <>
-      <div className="fondo">
-        {" "}
-        <input type="text" placeholder="Add Guest" />
-        <i className="fa-solid fa-magnifying-glass"></i>
-      </div>
+      <main>
+        {data.map((el, index) => {
+          return (
+            <div className="car">
+              <div className="DivImg">
+                <img src={el.photo} key={index}></img>
+              </div>
+              <div className="DivText">
+                {el.superHost ? (
+                  <p className="parr1">
+                    <strong>SuperHost</strong> {el.type}
+                    {" " + el.beds + " beds"}
+                    {" " + el.rating}
+                    <span class="material-symbols-outlined">star</span>
+                    <h3 className="tit">
+                      <strong>{el.title}</strong>
+                    </h3>
+                  </p>
+                ) : (
+                  <p className="parr2">
+                    {el.type}
+                    {" " + el.beds + " beds"}
+                    {" " + el.rating}
+                    <span class="material-symbols-outlined">star</span>
+                    <h3 className="tit">
+                      <strong>{el.title}</strong>
+                    </h3>
+                  </p>
+                )}
+              </div>
+            </div>
+          );
+        })}
+      </main>
     </>
   );
 }
